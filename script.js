f3bce4e4d91e127032a58aab077856fde375a3f6
Stockmayr Fabian <it180225@EDU.HTL-LEONDING.AC.AT>
2020-01-22 14:27:09 +0100

    task21 complete

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ document.getElementById("js").addEventListener('click', swap)
 
 
 function swap() {
+ if (document.getElementById("js").childNodes[0].nodeName != 'IMG') {
     document.getElementById("js").innerHTML = "<img src='img/js.png'>";
+ }
+ else{
+    document.getElementById("js").innerHTML = "JavaSript";
+ }
 }
